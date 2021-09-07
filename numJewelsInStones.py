a2c5304b642483ba95a4ef9fcf576a546d58275c
Shal <shalmika.elango@gmail.com>
2021-09-06 21:54:40 -0400

    Update numJewelsInStones.py

--- numJewelsInStones.py
+++ numJewelsInStones.py
@@ -17,3 +17,22 @@ class Solution(object):
 
  # Time: O(n*n)
  # Space: O(1)
+    
+    
+   def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        jewelsSet = set(jewels)
+        return sum(s in jewelsSet for s in stones)
+    
+    
+    # Time: O(len(jewels) + len(stones))
+    # Space: O(len(jewels)
+    
+    
+    
+    
+      def numJewelsInStones(self, J, S):
+        return sum(s in J for s in S)
+    
+    # Time: O(len(jewels) * len(stones)) 
+    # Space: O(1) 
+    
